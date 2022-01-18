2b77a7c8d1185ffaa9d2c84d788bedd2667b3a5d
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-18 14:21:28 +0100

    Update requete_7.sql

--- Requête SQL/requete_7.sql
+++ Requête SQL/requete_7.sql
@@ -1,11 +1,10 @@
 CREATE TEMP TABLE IF NOT EXISTS tmp_table_1t AS
 SELECT commune.code_departement as departement, count(*) as nb_vente_1t
 		FROM vente
-		JOIN bienimmo ON bienimmo.id_art = vente.id_art
-		JOIN commune ON commune.id_commune = bienimmo.id_commune
+		JOIN bienimmo USING (id_art)
+		JOIN commune USING (id_commune)
 		WHERE date_vente BETWEEN '2020-01-01' AND '2020-03-30'
 		GROUP BY commune.code_departement;
-SELECT * from tmp_table_1t;
 
 CREATE TEMP TABLE IF NOT EXISTS tmp_table_2t AS 
 SELECT commune.code_departement as departement, count(*) as nb_vente_2t
@@ -17,5 +16,5 @@ SELECT commune.code_departement as departement, count(*) as nb_vente_2t
 		
 SELECT departement, (nb_vente_2t - nb_vente_1t)/nb_vente_1t::real * 100 as taux_evolution
 FROM tmp_table_1t 
-NATURAL JOIN tmp_table_2t
-WHERE (nb_vente_2t - nb_vente_1t)/nb_vente_1t::real * 100 > 20
+JOIN tmp_table_2t USING (departement)
+WHERE (nb_vente_2t - nb_vente_1t)/nb_vente_1t::real * 100 > 20;
