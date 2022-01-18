a3c6294c407b68207e4d8b7aadb2319be7c34fd4
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-18 14:20:09 +0100

    Update requete_6.sql

--- Requête SQL/requete_6.sql
+++ Requête SQL/requete_6.sql
@@ -1,16 +1,3 @@
-/* TAUX D'EVOLUTION DU NOMBRE DE VENTE ENTRE LE 1ER TRIMESTRE ET LE 2EME TRIMSTRE 2020
-
-Aucune correction apportées 
-Remarque : J'ai essayé de trouver une méthode pour remplacer les SELECT imbriqués
-			par des table tmp mais je n'ai pas réussi à créer la jointure entre les 
-			2 tables temporaires. 
-
---> Si tu as la solution à ce problème je suis preneur !
-
-Sinon la requête ci-dessous fonctionne très bien
-
-*/
-
 SELECT ROUND((nb_vente_2t - nb_vente_1t) * 100.0 / nb_vente_1t,2) AS taux_evolution
 FROM 
 	(
@@ -25,7 +12,3 @@ FROM
 		WHERE date_vente BETWEEN '2020-04-01' AND '2020-06-30'
 	) as nb_vente_2t_tmp_table
 ;
-
-
-		
-/* Résultat de la requête : Taux_evolution = +4.57% de vente en plus au 2eme trimestre */
