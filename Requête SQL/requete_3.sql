1d0088a735ae7a261c619bc66790457dd79dcd92
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-18 14:25:51 +0100

    Update requete_3.sql

--- Requête SQL/requete_3.sql
+++ Requête SQL/requete_3.sql
@@ -1,7 +1,7 @@
-SELECT commune.cp_com as departement, AVG(prix / bienimmo.surfart) as prix_metre_carre 
+SELECT commune.code_departement as departement, AVG(prix / bienimmo.surfart) as prix_metre_carre 
 FROM vente
 JOIN bienimmo USING (id_art) 
 JOIN commune USING (id_commune)
-GROUP BY commune.cp_com 
+GROUP BY commune.code_departement
 ORDER BY prix_metre_carre DESC
 LIMIT 10;
