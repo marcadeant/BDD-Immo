7694db3097ac63fe3160057d51279dc30d7d77f0
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-19 16:55:20 +0100

    Update requete_6.sql

--- Requête SQL/requete_6.sql
+++ Requête SQL/requete_6.sql
@@ -1,14 +1,14 @@
-SELECT ROUND((nb_vente_2t - nb_vente_1t) * 100.0 / nb_vente_1t,2) AS taux_evolution
-FROM 
-	(
-		SELECT count(*) AS nb_vente_1t
+CREATE TEMP TABLE IF NOT EXISTS nb_vente_1t AS
+
+SELECT count(*) AS nb_vente_1t
 		FROM vente
-		WHERE date_vente BETWEEN '2020-01-01' AND '2020-03-30'
+		WHERE date_vente BETWEEN '2020-01-01' AND '2020-03-30';
 		
-	) as nb_vente_1t_tmp_table,
-	(
-		SELECT count(*) AS nb_vente_2t
+CREATE TEMP TABLE IF NOT EXISTS nb_vente_2t AS
+SELECT count(*) AS nb_vente_2t
 		FROM vente
-		WHERE date_vente BETWEEN '2020-04-01' AND '2020-06-30'
-	) as nb_vente_2t_tmp_table
-;
+		WHERE date_vente BETWEEN '2020-04-01' AND '2020-06-30';
+
+SELECT ROUND((nb_vente_2t - nb_vente_1t) * 100.0 / nb_vente_1t,2) AS taux_evolution
+FROM nb_vente_1t 
+CROSS JOIN nb_vente_2t;
