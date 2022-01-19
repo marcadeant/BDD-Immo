6d39d600db1cc37fe8a54f001340ad008083c924
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-19 17:17:09 +0100

    Update requete_8.sql

--- Requête SQL/requete_8.sql
+++ Requête SQL/requete_8.sql
@@ -1,24 +1,21 @@
-SELECT (p_3p.prix_moyen_metre2_3_pieces - p_2p.prix_moyen_metre2_2_pieces)/p_2p.prix_moyen_metre2_2_pieces * 100 
-AS diff_prix_3p_2p
-FROM (
-		SELECT AVG(prix) as prix_moyen_metre2_2_pieces
+CREATE TEMP TABLE p_2p AS
+
+SELECT AVG(prix) as prix_moyen_metre2_2_pieces
 		FROM vente
 		JOIN bienimmo USING (id_art)
 		WHERE bienimmo.type_local = 'Appartement' 
 		AND bienimmo.nb_piece = 2
-		GROUP BY (bienimmo.nb_piece)
+		GROUP BY (bienimmo.nb_piece);
 
-	) as p_2p,
-
-	(	
-
+CREATE TEMP TABLE p_3p AS 
 		SELECT AVG(prix) as prix_moyen_metre2_3_pieces
 		FROM vente
 		JOIN bienimmo USING (id_art)
 		WHERE bienimmo.type_local = 'Appartement' 
 		AND bienimmo.nb_piece = 3
-		GROUP BY (bienimmo.nb_piece)
+		GROUP BY (bienimmo.nb_piece);
 
-
-	) as p_3p;
+SELECT (p_3p.prix_moyen_metre2_3_pieces - p_2p.prix_moyen_metre2_2_pieces)/p_2p.prix_moyen_metre2_2_pieces * 100 
+FROM p_3p
+CROSS JOIN p_2p;
 
