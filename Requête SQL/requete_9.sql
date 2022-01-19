ff85d7b87ab165d11241a829e3f4958464ca46ec
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-19 16:22:51 +0100

    Update requete_9.sql

--- Requête SQL/requete_9.sql
+++ Requête SQL/requete_9.sql
@@ -1,60 +1,21 @@
-CREATE TEMP TABLE IF NOT EXISTS dep_6 AS
-SELECT commune.libellé as commune, commune.code_departement as departement, AVG(vente.prix) as prix_moyen
-FROM vente
+CREATE TEMP TABLE prix_moyen_commune AS
+SELECT commune.libellé as nom_commune, commune.code_departement as departement, AVG(prix) as prix_moyen
+FROM vente 
 JOIN bienimmo USING (id_art)
 JOIN commune USING (id_commune)
-WHERE commune.code_departement = 6
-GROUP BY (commune.libellé, commune.code_departement)
-ORDER BY prix_moyen DESC
-LIMIT 3;
+GROUP BY nom_commune, departement;
 
-CREATE TEMP TABLE IF NOT EXISTS dep_13 AS
-SELECT commune.libellé as commune, commune.code_departement as departement, AVG(vente.prix) as prix_moyen
-FROM vente
-JOIN bienimmo USING (id_art)
-JOIN commune USING (id_commune)
-WHERE commune.code_departement = 13
-GROUP BY (commune.libellé, commune.code_departement)
-ORDER BY prix_moyen DESC
-LIMIT 3;
+CREATE TEMP TABLE ranking_commune AS
 
-CREATE TEMP TABLE IF NOT EXISTS dep_33 AS
-SELECT commune.libellé as commune, commune.code_departement as departement, AVG(vente.prix) as prix_moyen
-FROM vente
-JOIN bienimmo USING (id_art)
-JOIN commune USING (id_commune)
-WHERE commune.code_departement = 33
-GROUP BY (commune.libellé, commune.code_departement)
-ORDER BY prix_moyen DESC
-LIMIT 3;
+SELECT  
+RANK() OVER (
 
-CREATE TEMP TABLE IF NOT EXISTS dep_59 AS
-SELECT commune.libellé as commune, commune.code_departement as departement, AVG(vente.prix) as prix_moyen
-FROM vente
-JOIN bienimmo USING (id_art)
-JOIN commune USING (id_commune)
-WHERE commune.code_departement = 59
-GROUP BY (commune.libellé, commune.code_departement)
-ORDER BY prix_moyen DESC
-LIMIT 3;
+		PARTITION BY departement
+		ORDER BY prix_moyen DESC
 
-CREATE TEMP TABLE IF NOT EXISTS dep_69 AS
-SELECT commune.libellé as commune, commune.code_departement as departement, AVG(vente.prix) as prix_moyen
-FROM vente
-JOIN bienimmo USING (id_art)
-JOIN commune USING (id_commune)
-WHERE commune.code_departement = 69
-GROUP BY (commune.libellé, commune.code_departement)
-ORDER BY prix_moyen DESC
-LIMIT 3;
+) as ranking, nom_commune, departement, prix_moyen		
+FROM prix_moyen_commune
+WHERE departement IN (6,13,33,59,69);
 
-
-SELECT * from dep_13
-UNION ALL
-SELECT * from dep_6
-UNION ALL 
-SELECT * from dep_33
-UNION ALL
-SELECT * from dep_59
-UNION ALL
-SELECT * from dep_69;
+SELECT * FROM ranking_commune
+WHERE ranking < 4
