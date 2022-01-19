c4810e8f99bc1d4059dfbad52a830de007ddc883
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-19 16:50:16 +0100

    Update requete_2.sql

--- Requête SQL/requete_2.sql
+++ Requête SQL/requete_2.sql
@@ -1,4 +1,4 @@
-SELECT bienimmo.nb_piece as nb_piece, count(id_vente) * 100 / (SELECT count(*) from vente)::real  as nb_vente 
+SELECT bienimmo.nb_piece as nb_piece, ROUND(count(id_vente) * 100.0 / (SELECT count(*) from vente),3)  as nb_vente 
 FROM vente 
 JOIN bienimmo USING (id_art)
 GROUP BY bienimmo.nb_piece
