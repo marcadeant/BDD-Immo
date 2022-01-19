31932d9da5ef04a06a9a5e84de1242b17d7f85be
marcadeant <32680233+marcadeant@users.noreply.github.com>
2022-01-19 17:32:14 +0100

    Update requete_8.sql

--- Requête SQL/requete_8.sql
+++ Requête SQL/requete_8.sql
@@ -1,4 +1,4 @@
-CREATE TEMP TABLE p_2p AS
+CREATE TEMP TABLE IF NOT EXISTS p_2p AS
 
 SELECT AVG(prix) as prix_moyen_metre2_2_pieces
 		FROM vente
@@ -7,7 +7,7 @@ SELECT AVG(prix) as prix_moyen_metre2_2_pieces
 		AND bienimmo.nb_piece = 2
 		GROUP BY (bienimmo.nb_piece);
 
-CREATE TEMP TABLE p_3p AS 
+CREATE TEMP TABLE IF NOT EXISTS p_3p AS 
 		SELECT AVG(prix) as prix_moyen_metre2_3_pieces
 		FROM vente
 		JOIN bienimmo USING (id_art)
@@ -15,7 +15,7 @@ CREATE TEMP TABLE p_3p AS
 		AND bienimmo.nb_piece = 3
 		GROUP BY (bienimmo.nb_piece);
 
-SELECT (p_3p.prix_moyen_metre2_3_pieces - p_2p.prix_moyen_metre2_2_pieces)/p_2p.prix_moyen_metre2_2_pieces * 100 
+SELECT ROUND(((prix_moyen_metre2_3_pieces - prix_moyen_metre2_2_pieces) * 100/prix_moyen_metre2_2_pieces)::numeric, 2)
 FROM p_3p
 CROSS JOIN p_2p;
 
